397e678dccafe3d3774beba271815fc6b7c3aacf
Tofutush <xiexuanqisirocco@163.com>
2025-01-28 22:32:20 +0800

    quotabar

--- src/App.svelte
+++ src/App.svelte
@@ -2,10 +2,11 @@
     import Prisoner from "./components/Prisoner.svelte";
     import { gameState } from "./classes/gameState.svelte";
     import QuotaMeter from "./components/QuotaMeter.svelte";
+    import QuotaBar from "./components/QuotaBar.svelte";
 </script>
 
 <main>
-    <QuotaMeter />
+    <QuotaBar />
     <p>Rocks: {gameState.rocks}</p>
     <Prisoner name="Tomato" />
     <Prisoner name="Tephra" />
---
+++ src/components/QuotaBar.svelte
@@ -0,0 +1,18 @@
+<script>
+    import { gameState } from "../classes/gameState.svelte";
+    import QuotaMeter from "./QuotaMeter.svelte";
+
+    function submit() {
+        if (gameState.rocks >= gameState.quota) {
+            gameState.rocks -= gameState.quota;
+            gameState.level++;
+            gameState.quota *= 1.5;
+        }
+    }
+</script>
+
+<div class="quota-bar">
+    <p>Camp level: {gameState.level}</p>
+    <QuotaMeter />
+    <button onclick={submit}>Submit</button>
+</div>
