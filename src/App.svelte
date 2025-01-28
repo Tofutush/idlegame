49d929052f37b4b7590a9f52c22bec2a25a939cc
Tofutush <xiexuanqisirocco@163.com>
2025-01-28 15:24:49 +0800

    space indent

--- src/App.svelte
+++ src/App.svelte
@@ -1,14 +1,14 @@
 <script lang="ts">
-	import Prisoner from "./components/Prisoner.svelte";
-	import { gameState } from "./classes/gameState.svelte";
+    import Prisoner from "./components/Prisoner.svelte";
+    import { gameState } from "./classes/gameState.svelte";
 </script>
 
 <main>
-	<p>Rocks: {gameState.rocks}</p>
-	<Prisoner />
-	<Prisoner />
-	<Prisoner />
-	<Prisoner />
+    <p>Rocks: {gameState.rocks}</p>
+    <Prisoner />
+    <Prisoner />
+    <Prisoner />
+    <Prisoner />
 </main>
 
 <style>
--- src/components/Prisoner.svelte
+++ src/components/Prisoner.svelte
@@ -1,8 +1,8 @@
 <script lang="ts">
-	import { gameState } from "../classes/gameState.svelte";
-	function addRock() {
-		gameState.rocks += 1;
-	}
+    import { gameState } from "../classes/gameState.svelte";
+    function addRock() {
+        gameState.rocks += 1;
+    }
 </script>
 
 <button onclick={addRock}>Dig</button>
