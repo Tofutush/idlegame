27732829651d3b65e0e1e8910e696bf1bfe299cc
Tofutush <xiexuanqisirocco@163.com>
2025-01-28 15:12:58 +0800

    basic rocks

---
+++ src/gameState.svelte.ts
@@ -0,0 +1,16 @@
+interface GameState {
+	rocks: number;
+	bucks: number;
+	level: number;
+	quota: number;
+	prisoners: number;
+	guards: number;
+}
+export const gameState: GameState = $state({
+	rocks: 0,
+	bucks: 0,
+	level: 1,
+	quota: 100,
+	prisoners: 0,
+	guards: 0
+});
---
+++ src/lib/Prisoner.svelte
@@ -0,0 +1,8 @@
+<script lang="ts">
+	import { gameState } from "../gameState.svelte";
+	function addRock() {
+		gameState.rocks += 1;
+	}
+</script>
+
+<button onclick={addRock}>Dig</button>
