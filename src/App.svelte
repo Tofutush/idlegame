e2070ff3e5d377a470d13b29259312ba1d56e501
Tofutush <xiexuanqisirocco@163.com>
2025-01-28 16:20:51 +0800

    quota meter

--- src/App.svelte
+++ src/App.svelte
@@ -1,9 +1,11 @@
 <script lang="ts">
     import Prisoner from "./components/Prisoner.svelte";
     import { gameState } from "./classes/gameState.svelte";
+    import QuotaMeter from "./components/QuotaMeter.svelte";
 </script>
 
 <main>
+    <QuotaMeter />
     <p>Rocks: {gameState.rocks}</p>
     <Prisoner />
     <Prisoner />
---
+++ src/components/QuotaMeter.svelte
@@ -0,0 +1,22 @@
+<script lang="ts">
+    import { gameState } from "../classes/gameState.svelte";
+    let progress = Math.min(gameState.rocks / gameState.quota, 1);
+    let filledQuotaWidth = $derived(progress * 500 + "px");
+    $inspect(filledQuotaWidth);
+</script>
+
+<div class="quota-meter">
+    <div class="filled-quota" style="width: {filledQuotaWidth}"></div>
+</div>
+
+<style>
+    .quota-meter {
+        width: 500px;
+        height: 20px;
+        background-color: #fff;
+    }
+    .filled-quota {
+        height: 100%;
+        background-color: #0001;
+    }
+</style>
