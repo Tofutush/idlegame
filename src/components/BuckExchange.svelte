b28f9b04ba4b196352feb56cfcc0c6f5674e528e
Tofutush <xiexuanqisirocco@163.com>
2025-01-29 23:18:51 +0800

    sell rocks

--- src/components/BuckExchange.svelte
+++ src/components/BuckExchange.svelte
@@ -1,12 +1,26 @@
 <script>
     import gameState from "../classes/gameState.svelte";
+    import Modal from "./Modal.svelte";
+    let modalShown = false;
 
     function sellRocks() {
         gameState.bucks += gameState.rocks;
         gameState.rocks = 0;
     }
+
+    function showModal() {
+        modalShown = true;
+    }
+
+    function hideModal() {
+        modalShown = false;
+    }
 </script>
 
 <div class="buck-exchange">
-    <button onclick={sellRocks}>Sell rocks!</button>
+    <button onclick={showModal}>Sell rocks!</button>
 </div>
+
+<Modal show={modalShown} hide={hideModal}>
+    <p>Sell rocks!</p>
+</Modal>
