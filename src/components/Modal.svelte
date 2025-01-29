bf4321b16df3478f584f962dd046698519a732ae
Tofutush <xiexuanqisirocco@163.com>
2025-01-29 23:18:47 +0800

    modal

---
+++ src/components/Modal.svelte
@@ -0,0 +1,26 @@
+<script>
+    let { show, hide, children } = $props();
+</script>
+
+{#if show}
+    <div class="modal-bg">
+        <div class="modal">
+            <button onclick={hide}>x</button>
+            {@render children()}
+        </div>
+    </div>
+{/if}
+
+<style>
+    .modal-bg {
+        width: 100%;
+        height: 100%;
+        background-color: #0001;
+        position: fixed;
+        top: 0;
+        left: 0;
+    }
+    .modal {
+        background-color: #fff;
+    }
+</style>
