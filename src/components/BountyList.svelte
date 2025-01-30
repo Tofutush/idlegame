862e4c1e7204b98131859b4e7f8a442939aa40f7
Tofutush <xiexuanqisirocco@163.com>
2025-01-31 01:30:56 +0800

    bounty list

---
+++ src/components/BountyList.svelte
@@ -0,0 +1,14 @@
+<script lang="ts">
+    import { getNotHavePrisonersList } from '../utils';
+
+    let notHadPrisoners = getNotHavePrisonersList();
+</script>
+
+<div class="bounty-list">
+    {#each notHadPrisoners as p}
+        <div class="bounty">
+            <h3>{p.name}</h3>
+            <p>Will 100% catch if bounty is {p.bounty} bucks.</p>
+        </div>
+    {/each}
+</div>
