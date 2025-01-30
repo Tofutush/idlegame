0f63090de5feab18f62873230d161ad3dc2c6d91
Tofutush <xiexuanqisirocco@163.com>
2025-01-31 01:27:02 +0800

    character identical to

--- src/classes/Character.ts
+++ src/classes/Character.ts
@@ -6,5 +6,9 @@ class Character {
         this.name = props.name
         this.color = props.color
     }
+    identicalTo(another: Character) {
+        // only checks name! dont fucking make 2 characters of the same name. please
+        return this.name === another.name;
+    }
 }
 export default Character;
