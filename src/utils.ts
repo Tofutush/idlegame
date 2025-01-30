c8677312dca4ea1a5f68dacc8472ce29365d9cd3
Tofutush <xiexuanqisirocco@163.com>
2025-01-31 01:28:06 +0800

    change to class list

--- src/utils.ts
+++ src/utils.ts
@@ -1,21 +1,21 @@
 import gameState from './classes/gameState.svelte';
-import prisonerList from './data/prisoners.json';
+import prisonerClassList from './data/prisonersClasses';
 
 function findPrisoner(name: string) {
-    for (let z = 0; z < prisonerList.length; z++) {
-        if (prisonerList[z].name === name) {
-            return prisonerList[z];
+    for (let z = 0; z < prisonerClassList.length; z++) {
+        if (prisonerClassList[z].name === name) {
+            return prisonerClassList[z];
         }
     }
     throw new Error(`prisoner ${name} not found`);
 }
 
 function getPrisonersList() {
-    return prisonerList.filter(p => gameState.prisoners.includes(p.name));
+    return prisonerClassList.filter(p => gameState.prisoners.includes(p.name));
 }
 
 function getNotHavePrisonersList() {
-    return prisonerList.filter(p => !gameState.prisoners.includes(p.name));
+    return prisonerClassList.filter(p => !gameState.prisoners.includes(p.name));
 }
 
 function getRandomInArray(arr: Array<any>) {
