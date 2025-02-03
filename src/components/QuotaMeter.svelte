7b67053f4df237b5bf8d6f4b3223e33c53fc5318
Tofutush <xiexuanqisirocco@163.com>
2025-02-03 14:47:34 +0800

    colors

--- src/components/QuotaMeter.svelte
+++ src/components/QuotaMeter.svelte
@@ -3,14 +3,14 @@
     let progress = $derived(Math.min(gameState.rocks / gameState.quota, 1) * 100);
 </script>
 
-<div class="quota-meter" style="background: linear-gradient(to right, var(--c) {progress}%, transparent {progress}%);">
+<div class="quota-meter" style="background: linear-gradient(to right, var(--gray) {progress}%, transparent {progress}%);">
     <div class="quota-progress">{gameState.rocks} / {gameState.quota}</div>
 </div>
 
 <style>
     .quota-meter {
         width: 480px;
-        border: 4px solid var(--c);
+        border: 4px solid var(--gray);
         border-radius: 12px;
         overflow: hidden;
         display: flex;
