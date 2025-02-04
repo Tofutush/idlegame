0eac8a87091211dd4df315400418a81e4a346244
Tofutush <xiexuanqisirocco@163.com>
2025-02-04 20:04:02 +0800

    dig animation

--- src/components/Prisoner.svelte
+++ src/components/Prisoner.svelte
@@ -14,13 +14,20 @@
     let bubbleShown = $state(false);
     let speech = $state('');
     let lastSpeech = $state('');
-    let image = $state(img);
+    let isDigging = $state(false);
 
     // events
     function levelUp() {
         if (gameState.bucks >= p.getLevelUpCost()) {
             gameState.bucks -= p.getLevelUpCost();
             p.levelUp();
+            clearInterval(digGifInterval);
+            digGifInterval = setInterval(
+                () => {
+                    if (p.level !== 0) isDigging = !isDigging;
+                },
+                Math.max(50, 1000 / p.level),
+            );
         } else {
             toast.error('Not enough money!');
         }
@@ -35,6 +42,12 @@
         gameState.rocks += p.getAmountRocksPerSecond();
     }
     setInterval(autoDigRock, 1000);
+    let digGifInterval = setInterval(
+        () => {
+            if (p.level !== 0) isDigging = !isDigging;
+        },
+        Math.max(50, 1000 / p.level),
+    );
 
     let bubbleTimeout = setTimeout(() => (bubbleShown = false), 3000);
     function showSpeechBubble() {
@@ -48,8 +61,8 @@
 <div class="prisoner">
     <p>{p.name}</p>
     <SpeechBubble show={bubbleShown}>{speech}</SpeechBubble>
-    <img src={image} alt="placeholder" style="height: 200px" />
+    <img src={isDigging ? imgDig : img} alt="placeholder" style="height: 200px" />
     <p>Level: {p.level}</p>
-    <button class="clicky" onclick={digRock}>Dig</button>
+    <button class="clicky" onclick={digRock} onmousedown={() => (isDigging = true)} onmouseup={() => (isDigging = false)}>Dig</button>
     <button class="clicky" onclick={levelUp}>Level Up (cost: {p.getLevelUpCost()})</button>
 </div>
