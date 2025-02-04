f78294172570237edfa3b2854d76b4c22f0411ca
Tofutush <xiexuanqisirocco@163.com>
2025-02-04 17:07:42 +0800

    remove autoclicklevel in favor of just level

--- src/classes/Prisoner.ts
+++ src/classes/Prisoner.ts
@@ -5,31 +5,21 @@ class Prisoner extends Character {
     bounty: number;
     efficiency: number;
     levelUpCost: number;
-    autoClickCost: number;
     level: number;
-    autoClickLevel: number;
     speech: Array<string>;
     constructor(props: PrisonerProps) {
         super(props);
         this.bounty = props.bounty;
         this.efficiency = props.efficiency;
         this.levelUpCost = props.levelUpCost;
-        this.autoClickCost = props.autoClickCost;
         this.speech = props.speech;
         this.level = props.level || 1;
-        this.autoClickLevel = props.autoClickLevel || 0;
-    }
-    getAmountRocksPerClick(): number {
-        return this.efficiency * this.level;
     }
     getAmountRocksPerSecond(): number {
-        return this.efficiency * this.autoClickLevel;
+        return this.efficiency * this.level;
     }
     levelUp(): void {
         this.level++;
-    }
-    levelAutoClickUp(): void {
-        this.autoClickLevel++;
     }
 }
 
--- src/components/Prisoner.svelte
+++ src/components/Prisoner.svelte
@@ -4,47 +4,31 @@
     import gameState from '../classes/gameState.svelte';
     import { getRandomInArray } from '../utils';
     import SpeechBubble from './SpeechBubble.svelte';
+    import type Prisoner from '../classes/Prisoner';
 
-    let { p } = $props();
+    let { p }: { p: Prisoner } = $props();
 
     // state vars
-    let level = $state(1);
-    let autoClickLevel = $state(0);
     let bubbleShown = $state(false);
     let speech = $state('');
 
-    // getters
-    function getRocksPerClick(): number {
-        return p.efficiency * level;
-    }
-
-    function getRocksPerSecond(): number {
-        return p.efficiency * autoClickLevel;
-    }
-
     // events
-    function digRock() {
-        gameState.rocks += getRocksPerClick();
-    }
     function levelUp() {
         if (gameState.bucks >= p.levelUpCost) {
             gameState.bucks -= p.levelUpCost;
-            level++;
-        } else {
-            toast.error('Not enough money!');
-        }
-    }
-    function levelAutoClickUp() {
-        if (gameState.bucks >= p.autoClickCost) {
-            gameState.bucks -= p.autoClickCost;
-            autoClickLevel++;
+            p.levelUp();
         } else {
             toast.error('Not enough money!');
         }
     }
 
+    function digRock() {
+        gameState.rocks += p.efficiency;
+        showSpeechBubble();
+    }
+
     function autoDigRock() {
-        gameState.rocks += getRocksPerSecond();
+        gameState.rocks += p.getAmountRocksPerSecond();
     }
     setInterval(autoDigRock, 1000);
 
@@ -61,14 +45,7 @@
     <p>{p.name}</p>
     <SpeechBubble show={bubbleShown}>{speech}</SpeechBubble>
     <img src={svelteLogo} alt="placeholder" />
-    <p>Level: {level}</p>
-    <p>Auto click: {autoClickLevel}</p>
-    <button
-        class="clicky"
-        onclick={() => {
-            digRock();
-            showSpeechBubble();
-        }}>Dig</button>
+    <p>Level: {p.level}</p>
+    <button class="clicky" onclick={digRock}>Dig</button>
     <button class="clicky" onclick={levelUp}>Level Up (cost: {p.levelUpCost})</button>
-    <button class="clicky" onclick={levelAutoClickUp}>Stop slacking (cost: {p.autoClickCost})</button>
 </div>
