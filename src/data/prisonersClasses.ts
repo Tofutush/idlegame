a7e49d0b0a9dac51c9bbda69f2f8257022144586
Tofutush <xiexuanqisirocco@163.com>
2025-02-05 21:32:38 +0800

    prisonerclasslist make no repeats

--- src/data/prisonersClasses.ts
+++ src/data/prisonersClasses.ts
@@ -4,6 +4,7 @@ import prisonerList from './prisoners.json';
 
 let prisonerClassList: Array<Prisoner> = [];
 for (let z = 0; z < prisonerList.length; z++) {
+    if (prisonerClassList.some(p => p.name === prisonerList[z].name)) throw new Error(`duplicate prisoners ${prisonerList[z].name}`);
     prisonerClassList.push(new Prisoner({
         efficiency: z + 1,
         bounty: Math.round(z == 0 ? 0 : 50 * Math.pow(1.4, z - 1)),
